7b7726e2e4f7af79b97839467a6b273799317b2d
YukiMurakami <muru31415926@gmail.com>
2023-05-04 11:22:39 +0900

    add player model and test

---
+++ hoshizukuri_game/models/player.py
@@ -0,0 +1,42 @@
+"""
+This module defines the Player model.
+"""
+from .pile import Pile, PileName, PileType
+from typing import Dict
+
+
+class Player:
+    """Card model class.
+
+    Args:
+        player_id (int): Player ID. 0 is the first player.
+
+    Attributes:
+        player_id (int): Player ID. 0 is the first player.
+        pile (Dict[PileName, Pile]): Player piles.
+        side_pile (Dict[str, SidePile]): Player side piles.
+        card_pool (Pile): The card list that player has.
+
+    """
+    def __init__(self, player_id: int):
+        self.player_id = player_id
+        self.pile: Dict[PileName, Pile] = {
+            PileName.HAND: Pile(PileType.LIST, card_list=[]),
+            PileName.FIELD: Pile(PileType.LIST, card_list=[]),
+            PileName.DECK: Pile(PileType.LIST, card_list=[]),
+            PileName.DISCARD: Pile(PileType.LIST, card_list=[]),
+            PileName.LOOK: Pile(PileType.LIST, card_list=[]),
+            PileName.REVEAL: Pile(PileType.LIST, card_list=[])
+        }
+
+    def get_status_json(self):
+        piles = {}
+        for pilename, pile in self.pile.items():
+            if len(pile.card_list) > 0:
+                piles[pilename.value] = [
+                    str(n) for n in pile.card_list
+                ]
+        return {
+            "player_id": self.player_id,
+            "pile": piles
+        }
---
+++ tests/models/test_player.py
@@ -0,0 +1,37 @@
+from hoshizukuri_game.models.player import Player
+from hoshizukuri_game.models.pile import Pile, PileName, PileType
+from hoshizukuri_game.models.card import Card
+
+
+class TestPlayer:
+    def test_get_status_json(self):
+        player = Player(player_id=1)
+        player.pile[PileName.DECK] = Pile(
+            PileType.LIST, card_list=[
+                Card(card_id=1, uniq_id=1),
+                Card(card_id=1, uniq_id=2),
+                Card(card_id=4, uniq_id=3)
+            ]
+        )
+        player.pile[PileName.HAND] = Pile(
+            PileType.LIST, card_list=[
+                Card(card_id=4, uniq_id=4),
+                Card(card_id=4, uniq_id=5)
+            ]
+        )
+        player.pile[PileName.FIELD] = Pile(
+            PileType.LIST, card_list=[
+                Card(card_id=1, uniq_id=6),
+                Card(card_id=1, uniq_id=7),
+                Card(card_id=2, uniq_id=8)
+            ]
+        )
+        data = player.get_status_json()
+        assert data == {
+            "player_id": 1,
+            "pile": {
+                "deck": ["1-1", "1-2", "4-3"],
+                "hand": ["4-4", "4-5"],
+                "field": ["1-6", "1-7", "2-8"]
+            }
+        }
