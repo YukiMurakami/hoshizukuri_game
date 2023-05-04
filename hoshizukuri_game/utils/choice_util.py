a3e1b14b1a1d0f02daad72f6ba2ba1208dd75d18
YukiMurakami <muru31415926@gmail.com>
2023-05-04 14:48:18 +0900

    add choice_util

---
+++ hoshizukuri_game/utils/choice_util.py
@@ -0,0 +1,90 @@
+"""
+This module defines the utility functions about choice.
+"""
+
+
+def cparseii(choice: str):
+    """
+    Parse the double int type choice.
+    (a:command:b-c)
+    Assumed that a is player ID, b is card ID, c is card uniq ID.
+    If c lacks, uniq iD will be -1.
+
+    Args:
+        choice (str): choice.
+
+    Returns:
+        List[Any]: n1, command, n2, n3  (if n3 is None, n3 = -1)
+    """
+    div = choice.split(":")
+    assert len(div) in [2, 3]
+    div2 = div[2].split("-")
+    card_id = int(div2[0])
+    uniq_id = -1
+    if len(div2) == 2:
+        uniq_id = int(div2[1])
+    return int(div[0]), div[1], card_id, uniq_id
+
+
+def cparsei(choice: str):
+    """
+    Parse the int type choice.
+    (a:command:b)
+    Assumed that a is player ID, b is card ID.
+
+    Args:
+        choice (str): choice.
+
+    Returns:
+        List[Any]: n1, command, n2.
+    """
+    div = choice.split(":")
+    assert len(div) == 3
+    return int(div[0]), div[1], int(div[2])
+
+
+def cparsell(choice: str):
+    """
+    Parse the list of double int type choice.
+    (a:command:b-c,d-e)
+    Assumed that a is player ID, b, d are card IDs, c, e are card uniq IDs.
+
+    Args:
+        choice (str): choice.
+
+    Returns:
+        List[Any]: n1, command, [n2, n3, ...], [n4, n5, ...].
+    """
+    div = choice.split(":")
+    card_ids = []
+    uniq_ids = []
+    for d_e in div[2].split(","):
+        if d_e == "":
+            continue
+        div2 = d_e.split("-")
+        card_ids.append(int(div2[0]))
+        if len(div2) == 2:
+            uniq_ids.append(int(div2[1]))
+    if len(card_ids) != len(uniq_ids):
+        uniq_ids = []
+    if card_ids == [0]:
+        card_ids = []
+    return int(div[0]), div[1], card_ids, uniq_ids
+
+
+def cparses(choice: str):
+    """
+    Parse the str type choice.
+    (a:command:param)
+    Assumed that a is player ID.
+
+    Args:
+        choice (str): choice.
+
+    Returns:
+        List[Any]: n1, command, param.
+    """
+    div = choice.split(":")
+    assert len(div) >= 3
+    param = ":".join(div[2:])
+    return int(div[0]), div[1], param
---
+++ tests/utils/test_choice_util.py
@@ -0,0 +1,69 @@
+from hoshizukuri_game.utils.choice_util import (
+    cparseii,
+    cparsei,
+    cparsell,
+    cparses,
+)
+
+
+class TestChoiceUtil:
+    def test_cparseii_1(self):
+        player_id, command, card_id, uniq_id = cparseii("1:play:8-2")
+        assert player_id == 1
+        assert command == "play"
+        assert card_id == 8
+        assert uniq_id == 2
+
+    def test_cparseii_2(self):
+        player_id, command, card_id, uniq_id = cparseii("1:play:8")
+        assert player_id == 1
+        assert command == "play"
+        assert card_id == 8
+        assert uniq_id == -1
+
+    def test_cparsei(self):
+        player_id, command, card_id = cparsei("1:play:8")
+        assert player_id == 1
+        assert command == "play"
+        assert card_id == 8
+
+    def test_cparsell_1(self):
+        player_id, command, card_ids, uniq_ids = cparsell("1:play:1-1,2-4")
+        assert player_id == 1
+        assert command == "play"
+        assert card_ids == [1, 2]
+        assert uniq_ids == [1, 4]
+
+    def test_cparsell_2(self):
+        player_id, command, card_ids, uniq_ids = cparsell("1:play:")
+        assert player_id == 1
+        assert command == "play"
+        assert card_ids == []
+        assert uniq_ids == []
+
+    def test_cparsell_3(self):
+        player_id, command, card_ids, uniq_ids = cparsell("1:play:1-1,2")
+        assert player_id == 1
+        assert command == "play"
+        assert card_ids == [1, 2]
+        assert uniq_ids == []
+
+    def test_cparsell_4(self):
+        player_id, command, card_ids, uniq_ids = cparsell("1:play:0")
+        assert player_id == 1
+        assert command == "play"
+        assert card_ids == []
+        assert uniq_ids == []
+
+    def test_cparses_1(self):
+        player_id, command, param = cparses("1:play:param1")
+        assert player_id == 1
+        assert command == "play"
+        assert param == "param1"
+
+    def test_cparses_2(self):
+        player_id, command, param = cparses(
+            "1:triggerselect:moatreaction:23-1")
+        assert player_id == 1
+        assert command == "triggerselect"
+        assert param == "moatreaction:23-1"
