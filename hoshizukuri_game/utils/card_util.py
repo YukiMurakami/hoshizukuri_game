5f5e4d09f3846e13ed7d47d4b49b470556411464
YukiMurakami <muru31415926@gmail.com>
2023-05-04 13:06:13 +0900

    add card_util

---
+++ hoshizukuri_game/utils/card_util.py
@@ -0,0 +1,282 @@
+"""
+This module defines the utility functions about carddata.
+"""
+from __future__ import annotations
+from typing import TYPE_CHECKING, List, Union, Dict
+if TYPE_CHECKING:
+    from ..models.card import Card
+    from ..models.pile import Pile
+    from ..models.game import Game
+import os
+import yaml
+from ..models.cost import Cost
+from ..models.card import CardType, CardColor
+
+
+class CardData:
+    singleton = None
+    filename = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), '../carddata.yaml')
+    cardinfo = {}
+    name2iddic = {}
+
+    def __new__(cls, *args, **kwargs):
+        if cls.singleton is None:
+            cls.singleton = super().__new__(cls)
+            cls.cardinfo, cls.name2iddic = cls._load_carddata(
+                cls.filename, cls._normalize_name
+            )
+        return cls.singleton
+
+    def _normalize_name(name):
+        """
+        Normalize card name.
+        This can remove spelling variants of cards.
+        """
+        name = name.lower()
+        name = name.replace(" ", "").replace("'", "").replace("-", "")
+        return name
+
+    def _load_carddata(filename, normalize_name_func):
+        cardinfo = {}
+        name2iddic = {}
+        with open(filename) as file:
+            data = yaml.safe_load(file)
+            for d in data:
+                name = d["name"]
+                jpn_name = d["japanese"]
+                norm_name = normalize_name_func(name)
+                cardinfo[d["id"]] = d
+                cardinfo[d["id"]]["orgname"] = name
+                cardinfo[d["id"]]["name"] = norm_name
+                cardinfo[d["id"]]["japanese"] = jpn_name
+                name2iddic[name] = d["id"]
+                name2iddic[norm_name] = d["id"]
+                name2iddic[jpn_name] = d["id"]
+                cardinfo[d["id"]]["type"] = [
+                    CardType(n) for n in d["type"].split("-")]
+                cardinfo[d["id"]]["cost"] = Cost(d["cost"])
+                cardinfo[d["id"]]["color"] = [
+                    CardColor(n) for n in d["color"].split("-")]
+        return cardinfo, name2iddic
+
+
+def get_card_id(name: str):
+    """
+    Get card ID with card name.
+
+    Args:
+        name (str): card name.
+
+    Returns:
+        int: Card ID.
+    """
+    return CardData().name2iddic[name]
+
+
+def get_original_name(card_id: int):
+    """
+    Get card origin name (without normalize) with card ID.
+
+    Args:
+        card_id (int): Card ID.
+
+    Returns:
+        str: Card original name.
+    """
+    return CardData().cardinfo[card_id]["orgname"]
+
+
+def get_expansion(card_id: int):
+    """
+    Get expansion with card ID.
+
+    Args:
+        card_id (int): Card ID.
+
+    Returns:
+        str: Expansion name.
+    """
+    return CardData().cardinfo[card_id]["expansion"]
+
+
+def get_types(card_id: int, game: Game = None):
+    """
+    Get card types with card ID.
+    If pileflag is True, this returns pile types. (Randomizer's card type)
+
+    Args:
+        card_id (int): Card ID.
+        game (Game, optional): Game. This is for checking game status.
+            (Inheritance, .etc)
+
+    Returns:
+        List[CardType]: List of card types.
+    """
+    return CardData().cardinfo[card_id]["type"]
+
+
+def is_create(card_id: int):
+    """
+    Check if card_id is create.
+
+    Args:
+        card_id (int): Card ID.
+
+    Returns:
+        boolean: Create is True.
+    """
+    return CardData().cardinfo[card_id]["create"]
+
+
+def id2uniq_id(pile: Pile, card_id: int, game: Game):
+    """
+    Get unique ID of the card which has card_id in pile.
+    If there are many cards which has card_id,
+    the first unique ID will be return.
+
+    Args:
+        pile (Pile): Only PileType.LIST.
+        card_id (int): Target card ID.
+        game (Game): now game.
+
+    Returns:
+        int: Unique card ID.
+
+    Note:
+        - When game has log_manager, need to process hidden cards.
+        - NO hidden cards are processed before hidden cards.
+            - If not found, complete from card pools.
+        - About hidden cards, the NO hidden cards are no longer guaranteed.
+            - For each card IDs, it should be 9999 (hidden card id).
+            - pile=[1,2,3,3], card_id=9999 -> pile will be [9999,9999,9999,3]
+    """
+    uniq_ids = ids2uniq_ids(pile, [card_id], game)
+    return uniq_ids[0]
+
+
+def ids2uniq_ids(pile: Pile, card_ids: List[int], game: Game):
+    """
+    Get unique IDs of the cards which have card_ids in pile.
+
+    Args:
+        pile (Pile): Only PileType.LIST.
+        card_ids (List[int]): Target card IDs.
+        game (Game): now game.
+
+    Returns:
+        List[int]: Unique card IDs.
+    """
+    result = []
+    already_uniq_ids = []
+    for card_id in card_ids:
+        hit = False
+        for card in pile.card_list:
+            if card.id == card_id and card.uniq_id not in already_uniq_ids:
+                result.append(card.uniq_id)
+                already_uniq_ids.append(card.uniq_id)
+                hit = True
+                break
+        if not hit:
+            raise Exception("Not found card_id: %d" % card_id)
+    return result
+
+
+def ids2cards(pile: Pile, card_ids: List[int], game: Game) -> List[Card]:
+    """
+    Get Card list which have card_ids in pile.
+
+    Args:
+        pile (Pile): Only PileType.LIST.
+        card_ids (List[int]): Target card IDs.
+        game (Game): now game.
+
+    Returns:
+        List[Card]: Card list.
+    """
+    uniq_ids = ids2uniq_ids(pile, card_ids, game)
+    result = []
+    for uniq_id in uniq_ids:
+        for card in pile.card_list:
+            if card.uniq_id == uniq_id:
+                result.append(card)
+                break
+    return result
+
+
+def get_cost(card_id: int, game: Game) -> Cost:
+    """
+    Get cost of card ID.
+
+    Args:
+        card_id (int): Card ID.
+        game (Game): now game.
+
+    Returns:
+        Cost: cost of the card.
+    """
+    cost: Cost = CardData().cardinfo[card_id]["cost"]
+    return cost
+
+
+def get_count(
+        pile_or_piles: Union[Pile, List[Pile], Dict[int, Pile]],
+        card_id: int):
+    """
+    Get the number of specific cards in pile.
+
+    Args:
+        pile_or_piles (Pile or List[Pile] or Dict[int, Pile]): Target pile.
+        card_id (int): Card ID to count.
+
+    Returns:
+        int: the number of cards.
+    """
+    def sub_count(pile: Pile, card_id: int):
+        count = 0
+        if pile.type.value == "list":
+            for card in pile.card_list:
+                if card.id == card_id:
+                    count += 1
+        elif pile.type.value == "listlist":
+            for card_list in pile.card_list:
+                for card in card_list:
+                    if card.id == card_id:
+                        count += 1
+        else:
+            if pile.pile_card_id == card_id:
+                count = pile.count
+        return count
+
+    if isinstance(pile_or_piles, list):
+        count = 0
+        for pile in pile_or_piles:
+            count += sub_count(pile, card_id)
+        return count
+    if isinstance(pile_or_piles, dict):
+        count = 0
+        for pile in pile_or_piles.values():
+            count += sub_count(pile, card_id)
+        return count
+    return sub_count(pile_or_piles, card_id)
+
+
+def is_same_card_ids(a: List[int], b: List[int]):
+    """
+    Compare between two card IDs.
+
+    Args:
+        a (List[int]): card IDs.
+        b (List[int]): card IDs.
+
+    Returns:
+        bool: True is same.
+    """
+    if len(a) != len(b):
+        return False
+    sort_a = sorted(a)
+    sort_b = sorted(b)
+    for i in range(len(sort_a)):
+        if sort_a[i] != sort_b[i]:
+            return False
+    return True
---
+++ tests/utils/test_card_util.py
@@ -0,0 +1,160 @@
+from hoshizukuri_game.utils.card_util import (
+    get_card_id,
+    get_expansion,
+    get_original_name,
+    get_types,
+    ids2cards,
+    is_same_card_ids,
+    id2uniq_id,
+    ids2uniq_ids,
+    get_cost,
+    get_count,
+    is_create,
+)
+from hoshizukuri_game.models.game import Game
+from hoshizukuri_game.models.card import CardType, Card
+from hoshizukuri_game.models.pile import Pile, PileType
+from hoshizukuri_game.models.cost import Cost
+import pytest
+
+
+class TestCardUtil:
+    def test_get_card_id_1(self):
+        result = get_card_id("stardust")
+        assert result == 1
+
+    def test_get_card_id_2(self):
+        result = get_card_id("Rock")
+        assert result == 2
+
+    def test_get_card_id_3(self):
+        result = get_card_id("衛星")
+        assert result == 3
+
+    def test_get_expansion(self):
+        result = get_expansion(10)
+        assert result == "Base"
+
+    def test_get_types(self):
+        game = Game()
+        result = get_types(get_card_id("satellite"), game)
+        assert result == [CardType.STAR, CardType.INITIAL]
+
+    def test_id2uniqid_1(self):
+        card_id = 8
+        pile = Pile(PileType.LIST, card_list=[
+            Card(4, 0), Card(4, 1), Card(1, 2),
+            Card(8, 3), Card(8, 4)
+        ])
+        game = Game()
+        uniq_id = id2uniq_id(pile, card_id, game)
+        assert uniq_id == 3
+
+    def test_id2uniqid_2(self):
+        card_id = 9
+        pile = Pile(PileType.LIST, card_list=[
+            Card(4, 0), Card(4, 1), Card(1, 2),
+            Card(8, 3), Card(8, 4)
+        ])
+        game = Game()
+        with pytest.raises(Exception):
+            id2uniq_id(pile, card_id, game)
+
+    def test_ids2uniqids_1(self):
+        card_ids = [4, 4]
+        pile = Pile(PileType.LIST, card_list=[
+            Card(4, 0), Card(4, 1), Card(1, 2),
+            Card(8, 3), Card(8, 4)
+        ])
+        game = Game()
+        uniq_ids = ids2uniq_ids(pile, card_ids, game)
+        assert uniq_ids == [0, 1]
+
+    def test_ids2uniqids_2(self):
+        card_ids = [4, 7]
+        pile = Pile(PileType.LIST, card_list=[
+            Card(4, 0), Card(4, 1), Card(1, 2),
+            Card(8, 3), Card(8, 4)
+        ])
+        game = Game()
+        with pytest.raises(Exception):
+            ids2uniq_ids(pile, card_ids, game)
+
+    def test_get_cost_1(self):
+        game = Game()
+        cost = get_cost(get_card_id("star"), game)
+        assert cost == Cost(10)
+
+    def test_ids2cards(self):
+        pile = Pile(PileType.LIST, card_list=[
+            Card(1, 1), Card(1, 2), Card(2, 3), Card(1, 4)
+        ])
+        game = Game()
+        cards = ids2cards(pile, card_ids=[1, 2], game=game)
+        assert len(cards) == 2
+        assert cards[0].id == 1
+        assert cards[0].uniq_id == 1
+        assert cards[1].id == 2
+        assert cards[1].uniq_id == 3
+
+    def test_get_count_1(self):
+        pile = Pile(
+            PileType.LIST, card_list=[
+                Card(1, 1), Card(1, 2), Card(2, 3), Card(1, 4)
+            ]
+        )
+        assert get_count(pile, 1) == 3
+
+    def test_get_count_2(self):
+        pile = Pile(
+            PileType.NUMBER, card_id_and_count=[1, 8]
+        )
+        assert get_count(pile, 1) == 8
+
+    def test_get_count_3(self):
+        piles = [
+            Pile(PileType.NUMBER, card_id_and_count=[1, 3]),
+            Pile(PileType.NUMBER, card_id_and_count=[2, 4]),
+            Pile(PileType.NUMBER, card_id_and_count=[3, 5])
+        ]
+        assert get_count(piles, 2) == 4
+
+    def test_get_count_4(self):
+        piles = {
+            1: Pile(PileType.NUMBER, card_id_and_count=[1, 3]),
+            2: Pile(PileType.NUMBER, card_id_and_count=[2, 4]),
+            3: Pile(PileType.NUMBER, card_id_and_count=[3, 5])
+        }
+        assert get_count(piles, 2) == 4
+
+    def test_get_count_5(self):
+        card_list = [
+            [
+                Card(card_id=2, uniq_id=1),
+            ],
+            [
+                Card(card_id=2, uniq_id=2),
+                Card(card_id=2, uniq_id=3),
+                Card(card_id=3, uniq_id=4),
+            ]
+        ]
+        pile = Pile(PileType.LISTLIST, card_list=card_list)
+        assert get_count(pile, 2) == 3
+
+    def test_is_same_card_ids_1(self):
+        assert is_same_card_ids([1, 2], [2, 1])
+
+    def test_is_same_card_ids_2(self):
+        assert is_same_card_ids([1, 2], [2]) is False
+
+    def test_is_same_card_ids_3(self):
+        assert is_same_card_ids([1, 2], [2, 2]) is False
+
+    def test_get_original_name(self):
+        assert get_original_name(1) == "Stardust"
+
+    def test_is_create_1(self):
+        assert is_create(1) is False
+
+    def test_is_create_2(self):
+        assert is_create(3)
