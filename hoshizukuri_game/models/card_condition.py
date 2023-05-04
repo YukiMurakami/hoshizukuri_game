f3af038ec63b8e893399fa6c556f3469b675ad71
YukiMurakami <muru31415926@gmail.com>
2023-05-04 15:57:02 +0900

    add CardCondition model and test

---
+++ hoshizukuri_game/models/card_condition.py
@@ -0,0 +1,173 @@
+"""
+This module defines the CardCondition model.
+"""
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict, List, Union
+if TYPE_CHECKING:
+    from ..models.game import Game
+from .card import CardType, Card
+from .cost import Cost
+from .pile import Pile, PileType
+from ..utils.card_util import (
+    get_types, get_cost, is_create,
+)
+
+
+class CardCondition:
+    """
+    CardCondition model class.
+
+    Args:
+        card_id (int): Must be this card ID.
+        uniq_id (int): Must be this unique card ID.
+        card_ids (List[int]): Must be included in this card ID list.
+        le_cost (Cost): Must be less equal than this cost.
+        eq_cost (Cost): Must be equal this cost.
+        type (CardType): Must be this card type.
+        create (bool): this have to have "create".
+
+    TODO:
+        Cost, etc.
+    """
+    def __init__(
+            self, card_id: int = None,
+            uniq_id: int = None,
+            card_ids: List[int] = None,
+            le_cost: Cost = None,
+            eq_cost: Cost = None,
+            type: CardType = None,
+            create: bool = None):
+        self.card_id = card_id
+        self.uniq_id = uniq_id
+        self.card_ids = card_ids
+        self.le_cost = le_cost
+        self.eq_cost = eq_cost
+        self.type = type
+        self.create = create
+
+    def __str__(self):
+        strings = []
+        if self.card_id is not None:
+            strings.append("id=%d" % self.card_id)
+        if self.uniq_id is not None:
+            strings.append("uniq=%d" % self.uniq_id)
+        if self.card_ids is not None:
+            strings.append("ids=[%s]" % ",".join(
+                [str(n) for n in self.card_ids]))
+        if self.le_cost is not None:
+            strings.append("cost<=%s" % str(self.le_cost))
+        if self.eq_cost is not None:
+            strings.append("cost=%s" % str(self.eq_cost))
+        if self.type is not None:
+            strings.append("type=%s" % self.type.value)
+        if self.create is not None:
+            strings.append("create=%s" % self.create)
+        return ",".join(strings)
+
+
+class CardConditionOr(CardCondition):
+    """
+    CardConditionOr model class.
+
+    Args:
+        conditions (List[CardCondition]): card conditions.
+    """
+    def __init__(self, conditions: List[CardCondition]):
+        self.conditions = conditions
+
+    def __str__(self):
+        return "/".join([str(n) for n in self.conditions])
+
+
+def get_match_card_ids(
+        pile_or_piles: Union[Pile, List[Pile], Dict[int, Pile]],
+        condition: CardCondition,
+        game: Game, uniq_flag: bool = False):
+    """
+    Get list of card ids satisfied condition.
+
+    Args:
+        pile_or_piles (Pile or List[Pile] or Dict[int, Pile]): Target pile.
+        condition (CardCondition): Condition.
+        game (Game): Now game.
+        uniq_flag (bool): True is for unique list.
+
+    Returns:
+        list[int]: List of card ids satisfied condition.
+    """
+    if isinstance(pile_or_piles, list):
+        result = []
+        for pile in pile_or_piles:
+            result += get_match_card_ids(
+                pile, condition, game, uniq_flag=uniq_flag)
+        if uniq_flag:
+            result = list(set(result))
+        result = sorted(result)
+        return result
+    if isinstance(pile_or_piles, dict):
+        result = []
+        for pile in pile_or_piles.values():
+            result += get_match_card_ids(
+                pile, condition, game, uniq_flag=uniq_flag)
+        if uniq_flag:
+            result = list(set(result))
+        result = sorted(result)
+        return result
+    assert isinstance(pile_or_piles, Pile)
+    pile = pile_or_piles
+    result = []
+    if pile.type == PileType.LIST:
+        for card in pile.card_list:
+            if is_match_card(card, condition, game):
+                result.append(card.id)
+    else:
+        for _ in range(pile.count):
+            if is_match_card(Card(pile.pile_card_id, -1), condition, game):
+                result.append(pile.pile_card_id)
+    if uniq_flag:
+        result = list(set(result))
+    result = sorted(result)
+    return result
+
+
+def is_match_card(
+        card: Card, condition: CardCondition, game: Game):
+    """
+    Check if card satisfies condition.
+
+    Args:
+        card (Card): Target card.
+        condition (CardCondition): Condition.
+        game (Game): Now game.
+
+    Returns:
+        boolean: If target card satisfies condition, True.
+    """
+    if isinstance(condition, CardConditionOr):
+        for cond in condition.conditions:
+            if is_match_card(card, cond, game):
+                return True
+        return False
+    if condition.card_id is not None:
+        if condition.card_id != card.id:
+            return False
+    if condition.uniq_id is not None:
+        if condition.uniq_id != card.uniq_id:
+            return False
+    if condition.card_ids is not None:
+        if card.id not in condition.card_ids:
+            return False
+    if condition.type is not None:
+        types = get_types(card.id, game=game)
+        if condition.type not in types:
+            return False
+    if condition.le_cost is not None:
+        if (get_cost(card.id, game) <= condition.le_cost) is False:
+            return False
+    if condition.eq_cost is not None:
+        if (get_cost(card.id, game) == condition.eq_cost) is False:
+            return False
+    if condition.create is not None:
+        if condition.create != is_create(card.id):
+            return False
+    return True
---
+++ tests/models/test_card_condition.py
@@ -0,0 +1,210 @@
+from hoshizukuri_game.models.card_condition import (
+    CardCondition,
+    CardConditionOr,
+    is_match_card,
+    get_match_card_ids
+)
+from hoshizukuri_game.models.cost import Cost
+from hoshizukuri_game.utils.card_util import get_card_id
+from hoshizukuri_game.models.card import Card, CardType
+from hoshizukuri_game.models.game import Game
+from hoshizukuri_game.models.pile import Pile, PileType
+
+
+class TestCardCondition:
+    def test_card_condition_str1(self):
+        cond = CardCondition()
+        assert str(cond) == ""
+
+    def test_card_condition_str2(self):
+        cond = CardCondition(card_id=1, uniq_id=3)
+        assert str(cond) == "id=1,uniq=3"
+
+    def test_card_condition_str3(self):
+        cond = CardCondition(type=CardType.INITIAL)
+        assert str(cond) == "type=initial"
+
+    def test_card_condition_str4(self):
+        cond = CardCondition(le_cost=Cost(3))
+        assert str(cond) == "cost<=3"
+
+    def test_card_condition_str5(self):
+        cond = CardCondition(eq_cost=Cost(3))
+        assert str(cond) == "cost=3"
+
+    def test_card_condition_str6(self):
+        cond = CardConditionOr([
+            CardCondition(eq_cost=Cost(2)),
+            CardCondition(eq_cost=Cost(3))])
+        assert str(cond) == "cost=2/cost=3"
+
+    def test_card_condition_str7(self):
+        cond = CardCondition(card_ids=[1, 2, 3])
+        assert str(cond) == "ids=[1,2,3]"
+
+    def test_card_condition_str8(self):
+        cond = CardCondition(create=True)
+        assert str(cond) == "create=True"
+
+    def test_card_condition_1(self):
+        game = Game()
+        cond = CardCondition(card_id=1)
+        card = Card(1, 100)
+        assert is_match_card(card, cond, game=game) is True
+
+    def test_card_condition_2(self):
+        game = Game()
+        cond = CardCondition(card_id=1)
+        card = Card(2, 100)
+        assert is_match_card(card, cond, game=game) is False
+
+    def test_card_condition_3(self):
+        game = Game()
+        cond = CardCondition(uniq_id=1)
+        card = Card(2, 100)
+        assert is_match_card(card, cond, game=game) is False
+
+    def test_card_condition_4(self):
+        game = Game()
+        cond = CardCondition(type=CardType.CELESTIAL)
+        card = Card(get_card_id("eruption"), 100)
+        assert is_match_card(card, cond, game=game) is True
+
+    def test_card_condition_5(self):
+        game = Game()
+        cond = CardCondition(type=CardType.CELESTIAL)
+        card = Card(get_card_id("star"), 100)
+        assert is_match_card(card, cond, game=game) is False
+
+    def test_card_condition_6(self):
+        game = Game()
+        cond = CardCondition(le_cost=Cost(3))
+        card = Card(get_card_id("eruption"), 100)
+        assert is_match_card(card, cond, game=game) is True
+
+    def test_card_condition_7(self):
+        game = Game()
+        cond = CardCondition(le_cost=Cost(2))
+        card = Card(get_card_id("eruption"), 100)
+        assert is_match_card(card, cond, game=game) is False
+
+    def test_card_condition_8(self):
+        game = Game()
+        cond = CardCondition(eq_cost=Cost(3))
+        card = Card(get_card_id("eruption"), 100)
+        assert is_match_card(card, cond, game=game) is True
+
+    def test_card_condition_9(self):
+        game = Game()
+        cond = CardCondition(eq_cost=Cost(3))
+        card = Card(get_card_id("forest"), 100)
+        assert is_match_card(card, cond, game=game) is False
+
+    def test_card_condition_10(self):
+        game = Game()
+        cond = CardConditionOr([
+            CardCondition(eq_cost=Cost(2)),
+            CardCondition(eq_cost=Cost(3))])
+        card1 = Card(get_card_id("forest"), 100)
+        card2 = Card(get_card_id("eruption"), 100)
+        card3 = Card(get_card_id("water"), 100)
+        assert is_match_card(card1, cond, game) is False
+        assert is_match_card(card2, cond, game) is True
+        assert is_match_card(card3, cond, game) is True
+
+    def test_card_condition_11(self):
+        game = Game()
+        cond = CardCondition(card_ids=[1, 2, 3])
+        assert is_match_card(Card(1, 10), cond, game) is True
+        assert is_match_card(Card(4, 5), cond, game) is False
+
+    def test_card_condition_12(self):
+        game = Game()
+        cond = CardCondition(create=True)
+        assert is_match_card(Card(
+            get_card_id("planet"), 10), cond, game) is True
+        assert is_match_card(Card(
+            get_card_id("stardust"), 5), cond, game) is False
+
+    def test_get_match_card_ids1(self):
+        game = Game()
+        cond = CardCondition(type=CardType.STAR)
+        pile = Pile(PileType.LIST, card_list=[
+            Card(1, 6),
+            Card(2, 7),
+            Card(3, 8),
+            Card(4, 9),
+            Card(4, 10)
+        ])
+        result = get_match_card_ids(pile, cond, game, uniq_flag=True)
+        assert result == [3, 4]
+
+    def test_get_match_card_ids2(self):
+        game = Game()
+        cond = CardCondition(type=CardType.CELESTIAL)
+        pile1 = Pile(PileType.LIST, card_list=[
+            Card(1, 1),
+            Card(6, 2),
+            Card(8, 3),
+            Card(1, 4),
+            Card(9, 5)
+        ])
+        pile2 = Pile(PileType.LIST, card_list=[
+            Card(1, 1),
+            Card(6, 2),
+            Card(10, 3),
+            Card(1, 4),
+            Card(10, 5)
+        ])
+        result = get_match_card_ids([pile1, pile2], cond, game)
+        assert result == [6, 6, 8, 9, 10, 10]
+
+    def test_get_match_card_ids3(self):
+        game = Game()
+        cond = CardCondition(type=CardType.CELESTIAL)
+        pile1 = Pile(PileType.LIST, card_list=[
+            Card(1, 1),
+            Card(6, 2),
+            Card(8, 3),
+            Card(1, 4),
+            Card(9, 5)
+        ])
+        pile2 = Pile(PileType.LIST, card_list=[
+            Card(1, 1),
+            Card(6, 2),
+            Card(10, 3),
+            Card(1, 4),
+            Card(10, 5)
+        ])
+        result = get_match_card_ids([pile1, pile2], cond, game, uniq_flag=True)
+        assert result == [6, 8, 9, 10]
+
+    def test_get_match_card_ids4(self):
+        game = Game()
+        cond = CardCondition(type=CardType.CELESTIAL)
+        piles = [
+            Pile(PileType.NUMBER, card_id_and_count=[
+                get_card_id("stardust"), 7]),
+            Pile(PileType.NUMBER, card_id_and_count=[
+                get_card_id("star"), 10]),
+            Pile(PileType.NUMBER, card_id_and_count=[
+                get_card_id("forest"), 10])
+        ]
+
+        result = get_match_card_ids(piles, cond, game, uniq_flag=True)
+        assert result == [11]
+
+    def test_get_match_card_ids5(self):
+        game = Game()
+        cond = CardCondition(type=CardType.CELESTIAL)
+        piles = {
+            get_card_id("stardust"): Pile(PileType.NUMBER, card_id_and_count=[
+                get_card_id("stardust"), 7]),
+            get_card_id("star"): Pile(PileType.NUMBER, card_id_and_count=[
+                get_card_id("star"), 10]),
+            get_card_id("forest"): Pile(PileType.NUMBER, card_id_and_count=[
+                get_card_id("forest"), 10])
+        }
+
+        result = get_match_card_ids(piles, cond, game, uniq_flag=True)
+        assert result == [11]
