25507429f204e231c0e92311e0275868e1456422
YukiMurakami <muru31415926@gmail.com>
2023-05-14 18:53:10 +0900

    add CallTriggerStep and test

---
+++ hoshizukuri_game/steps/common/call_trigger_step.py
@@ -0,0 +1,147 @@
+"""
+Steps for call triggers.
+"""
+from __future__ import annotations
+from typing import TYPE_CHECKING, List
+if TYPE_CHECKING:
+    from ...models.game import Game
+from ..abstract_step import AbstractStep
+from ...models.activate import (
+    TargetActivate,
+    is_match_activate
+)
+from ...models.limit import (
+    TargetLimit,
+    LimitTriggerActivate, LimitForever,
+)
+from ...models.trigger import (
+    get_called_triggers,
+    Trigger, remove_triggers
+)
+from ...models.variable import (
+    set_variable, VariableName
+)
+from ...utils.choice_util import cparses
+
+
+class CallTriggerStep(AbstractStep):
+    """
+    call triggers with target activates.
+
+    Args:
+        player_id (int): player who has added buy.
+        depth (int): Expected log hierarchy.
+        target_activates (List[TargetActivate]): Target Activates
+            to call triggers.
+    """
+    def __init__(
+            self, player_id: int, depth: int,
+            target_activates: List[TargetActivate]):
+        super().__init__()
+        self.player_id = player_id
+        self.depth = depth
+        self.target_activates = target_activates
+
+    def __str__(self):
+        return "%d:calltrigger:%d:[%s]" % (
+            self.depth, self.player_id,
+            ",".join([str(n) for n in self.target_activates]))
+
+    def process(self, game: Game):
+        auto_triggers, select_triggers = get_called_triggers(
+            self.target_activates, game, self
+        )
+        if len(auto_triggers) > 0:
+            # process automatically
+            for auto_trigger in auto_triggers:
+                _process_triggers(game, auto_trigger, self)
+            return [self] + [t.step for t in auto_triggers]
+        if len(select_triggers) <= 0:
+            # all trigger checks done
+            # cancel triggers which were not played
+            #   and has only LimitTriggerActivate.
+            cancel_triggers = []
+            if hasattr(game, "triggers"):
+                for trigger in game.triggers:
+                    for t_activate in self.target_activates:
+                        if is_match_activate(
+                                t_activate, trigger.activate, game):
+                            if not trigger.step.can_play_trigger(
+                                    game, t_activate):
+                                if isinstance(
+                                        trigger.limit, LimitTriggerActivate):
+                                    # cancel
+                                    cancel_triggers.append(trigger)
+            for trigger in cancel_triggers:
+                game.triggers.remove(trigger)
+            return []
+        return [self, _TriggerSelectStep(
+            self.player_id, self.depth, select_triggers, self
+        )]  # call trigger select step
+
+
+def _process_triggers(game: Game, trigger: Trigger, source_step: AbstractStep):
+    target_limit = TargetLimit(
+        LimitTriggerActivate, trigger_id=trigger.id
+    )
+    value = "%s-%s" % (source_step.step_id, trigger.step.step_id)
+    set_variable(
+        game, VariableName.DONE_TRIGGER_LIST, list, LimitForever(), value)
+    remove_triggers(game, target_limit)
+
+
+class _TriggerSelectStep(AbstractStep):
+    def __init__(
+            self, player_id: int, depth: int, triggers: List[Trigger],
+            source_step: AbstractStep):
+        super().__init__()
+        self.player_id = player_id
+        self.depth = depth
+        self.triggers = triggers
+        self.source_step = source_step
+
+    def __str__(self):
+        activate_str = ",".join(
+            [str(n.step.trigger_activate) for n in self.triggers]
+        )
+        return "%d:triggerselect:%d:%s" % (
+            self.depth, self.player_id, activate_str
+        )
+
+    def process(self, game: Game):
+        if game.choice == "":
+            self.candidates = []
+            cannot_pass_count = 0
+            all_can_pass = True
+            for trigger in self.triggers:
+                self.candidates.append(
+                    "%d:triggerselect:%s" % (
+                        self.player_id, trigger.step.get_trigger_name())
+                )
+                if not trigger.can_pass:
+                    all_can_pass = False
+                    cannot_pass_count += 1
+            if all_can_pass:
+                self.candidates.append(
+                    "%d:triggerselect:pass" % self.player_id
+                )
+            if cannot_pass_count == 1 and len(self.candidates) == 1:
+                # auto select trigger
+                game.choice = self.candidates[0]
+        if game.choice == "":
+            return [self]
+        else:
+            self.candidates = []
+        player_id, command, trigger_name = cparses(game.choice)
+        game.choice = ""
+        assert player_id == self.player_id
+        assert command == "triggerselect"
+        if trigger_name == "pass":
+            for trigger in self.triggers:
+                _process_triggers(game, trigger, self.source_step)
+            return []
+        index = [n.step.get_trigger_name() for n in self.triggers].index(
+            trigger_name)
+        assert index is not None
+        _process_triggers(game, self.triggers[index], self.source_step)
+        return [self.triggers[index].step]
---
+++ tests/steps/common/test_call_trigger_step.py
@@ -0,0 +1,193 @@
+from hoshizukuri_game.models.trigger import Trigger
+from hoshizukuri_game.steps.abstract_step import AbstractStep
+from hoshizukuri_game.steps.common.call_trigger_step import (
+    CallTriggerStep, _TriggerSelectStep,
+    _process_triggers
+)
+from hoshizukuri_game.models.activate import (
+    TargetActivate,
+    ActivatePlaysetEnd
+)
+from hoshizukuri_game.models.game import Game
+from hoshizukuri_game.models.player import Player
+from hoshizukuri_game.models.limit import (
+    LimitForever,
+    LimitTriggerActivate,
+)
+from hoshizukuri_game.steps.base.daichi_step import _DaichiTriggerStep
+from hoshizukuri_game.utils.card_util import get_card_id
+
+
+class TestCallTriggerStep:
+    def test_str(self):
+        step = CallTriggerStep(0, 0, [TargetActivate(
+            ActivatePlaysetEnd, player_id=0)])
+        assert str(step) == (
+            "0:calltrigger:0:[target_activate:"
+            "ActivatePlaysetEnd:player_id=0]"
+        )
+
+    def test_process_1(self, get_step_classes):
+        game = Game()
+        game.set_players([Player(0)])
+        source_step = AbstractStep()
+        source_step.player_id = 0
+        trigger_1 = Trigger(
+            LimitTriggerActivate(None),
+            ActivatePlaysetEnd(0),
+            source_step, can_pass=False, auto=True)
+        game.triggers = [trigger_1]
+        step = CallTriggerStep(0, 0, [TargetActivate(
+            ActivatePlaysetEnd, player_id=0)])
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == [
+            CallTriggerStep, AbstractStep
+        ]
+        assert next_steps[1].step_id == source_step.step_id
+        assert len(game.triggers) == 0
+
+    def test_process_2(self, get_step_classes):
+        game = Game()
+        game.set_players([Player(0)])
+        source_step = AbstractStep()
+        source_step.player_id = 0
+        trigger_1 = Trigger(
+            LimitTriggerActivate(None),
+            ActivatePlaysetEnd(0),
+            source_step, can_pass=False, auto=False)
+        game.triggers = [trigger_1]
+        step = CallTriggerStep(0, 0, [TargetActivate(
+            ActivatePlaysetEnd, player_id=0)])
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == [
+            CallTriggerStep, _TriggerSelectStep]
+
+    def test_process_3(self, get_step_classes):
+        game = Game()
+        game.set_players([Player(0)])
+        game.triggers = []
+        step = CallTriggerStep(0, 0, [TargetActivate(
+            ActivatePlaysetEnd, player_id=0)])
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == []
+
+    def test_process_4(self, get_step_classes, mocker):
+        source_step = AbstractStep()
+        source_step.player_id = 0
+        mocker.patch(
+            "hoshizukuri_game.steps.abstract_step."
+            "AbstractStep.can_play_trigger", return_value=False)
+        game = Game()
+        game.set_players([Player(0)])
+        game.triggers = [
+            Trigger(
+                LimitTriggerActivate(None),
+                ActivatePlaysetEnd(0),
+                source_step
+            )
+        ]
+        step = CallTriggerStep(0, 0, [
+            TargetActivate(ActivatePlaysetEnd, 0)
+        ])
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == []
+        assert len(game.triggers) == 0
+
+
+class TestProcessTriggers:
+    def test_1(self):
+        game = Game()
+        step = AbstractStep()
+        source_step = AbstractStep()
+        trigger = Trigger(
+            LimitTriggerActivate(None),
+            ActivatePlaysetEnd(0),
+            step
+        )
+        game.triggers = [trigger]
+        _process_triggers(game, trigger, source_step)
+        assert len(game.triggers) == 0
+
+
+class TestTriggerSelectStep:
+    def test_str(self):
+        source_step = AbstractStep()
+        trigger_step = AbstractStep()
+        trigger = Trigger(
+            LimitForever(),
+            ActivatePlaysetEnd(0), trigger_step)
+        trigger.step.trigger_activate = TargetActivate(
+            ActivatePlaysetEnd, 0)
+        step = _TriggerSelectStep(0, 0, [trigger], source_step)
+        assert str(step) == (
+            "0:triggerselect:0:target_activate:"
+            "ActivatePlaysetEnd:player_id=0")
+
+    def get_triggers(self, can_pass=True):
+        step_1 = _DaichiTriggerStep(0, 0, 0)
+        step_1.trigger_activate = TargetActivate(ActivatePlaysetEnd, 0)
+        step_2 = _DaichiTriggerStep(0, 0, 1)
+        step_2.trigger_activate = TargetActivate(ActivatePlaysetEnd, 0)
+        triggers = [
+            Trigger(
+                LimitForever(),
+                ActivatePlaysetEnd(0),
+                step_1, can_pass=can_pass
+            ),
+            Trigger(
+                LimitForever(),
+                ActivatePlaysetEnd(0),
+                step_2, can_pass=can_pass
+            )
+        ]
+        return triggers
+
+    def test_process_1(self, get_step_classes):
+        triggers = self.get_triggers()
+        source_step = AbstractStep()
+        step = _TriggerSelectStep(0, 0, triggers, source_step)
+        game = Game()
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == [_TriggerSelectStep]
+        assert next_steps[0].get_candidates(game) == [
+            "0:triggerselect:daichi:%d-0#0" % get_card_id("daichi"),
+            "0:triggerselect:daichi:%d-1#0" % get_card_id("daichi"),
+            "0:triggerselect:pass#0"
+        ]
+
+    def test_process_2(self, get_step_classes):
+        triggers = self.get_triggers()
+        source_step = AbstractStep()
+        step = _TriggerSelectStep(0, 0, triggers, source_step)
+        game = Game()
+        game.choice = "0:triggerselect:daichi:%d-1" % get_card_id("daichi")
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == [_DaichiTriggerStep]
+        assert next_steps[0].get_candidates(game) == []
+
+    def test_process_3(self, get_step_classes):
+        triggers = self.get_triggers()
+        source_step = AbstractStep()
+        step = _TriggerSelectStep(0, 0, triggers, source_step)
+        game = Game()
+        game.choice = "0:triggerselect:pass"
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == []
+        assert game.triggers == []
+
+    def test_process_4(self, get_step_classes):
+        step_1 = _DaichiTriggerStep(0, 0, 0)
+        step_1.trigger_activate = TargetActivate(ActivatePlaysetEnd, 0)
+        triggers = [
+            Trigger(
+                LimitForever(),
+                ActivatePlaysetEnd(0),
+                step_1, can_pass=False
+            )
+        ]
+        source_step = AbstractStep()
+        step = _TriggerSelectStep(0, 0, triggers, source_step)
+        game = Game()
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == [_DaichiTriggerStep]
+        assert next_steps[0].get_candidates(game) == []
