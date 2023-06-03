e3eb27674b0bbd544adb89de2209a39cd44ba1fe
YukiMurakami <muru31415926@gmail.com>
2023-06-03 12:22:26 +0900

    add log into ArashiStep and test

--- hoshizukuri_game/steps/base/arashi_step.py
+++ hoshizukuri_game/steps/base/arashi_step.py
@@ -7,6 +7,7 @@ if TYPE_CHECKING:
     from ...models.game import Game
 from ..abstract_step import AbstractStep
 from ...models.pile import PileName
+from ...models.log import LogCondition, Command
 from ..common.discard_step import DiscardStep
 from ...utils.choice_util import cparsei, is_included_candidates
 
@@ -34,6 +35,8 @@ class ArashiStep(AbstractStep):
                 PileName.FIELD].card_list) <= 1:
             return []
         candidates = self._create_candidates(game)
+        if game.log_manager is not None:
+            game.choice = self._log2choice(game)
         if game.choice == "" or not is_included_candidates(
                 game.choice, candidates):
             self.candidates = candidates
@@ -68,3 +71,24 @@ class ArashiStep(AbstractStep):
                 self.player_id, n
             ) for n in candidates + [-1]
         ]
+
+    def _log2choice(self, game: Game):
+        if not game.log_manager.has_logs():
+            return game.choice
+        log_condition = LogCondition(
+            Command.DISCARD_FROM_PLAYAREA, self.player_id, depth=self.depth)
+        log = game.log_manager.get_nextlog(
+            log_condition
+        )
+        if log is None:
+            return "%d:arashiindex:-1" % self.player_id
+        card_ids = log.card_ids
+        index = -1
+        for i, cardlist in enumerate(
+                game.players[self.player_id].pile[PileName.FIELD].card_list):
+            if sorted([n.id for n in cardlist]) == sorted(card_ids):
+                index = i
+                break
+        return "%d:arashiindex:%d" % (
+            self.player_id, index
+        )
--- tests/steps/base/test_arashi_step.py
+++ tests/steps/base/test_arashi_step.py
@@ -83,3 +83,62 @@ class TestArashiStep():
         )
         next_steps = step.process(game)
         assert get_step_classes(next_steps) == []
+
+    def test_process_log_1(self, get_step_classes, make_log_manager):
+        step = ArashiStep(0, 0, 0)
+        game = self.get_game(
+            [
+                [Card(1, 1), Card(2, 2), Card(3, 3)],
+                [Card(1, 4)],
+                [Card(2, 5)],
+                [Card(1, 6), Card(2, 7)],
+                [Card(9, 10)]
+            ]
+        )
+        game.log_manager = make_log_manager(
+            "A discards 星屑 from their playarea."
+        )
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == [DiscardStep]
+        assert next_steps[0].orbit_index == 1
+
+    def test_process_log_2(
+            self, get_step_classes, is_equal_candidates, make_log_manager):
+        step = ArashiStep(0, 0, 0)
+        game = self.get_game(
+            [
+                [Card(1, 1), Card(2, 2), Card(3, 3)],
+                [Card(1, 4)],
+                [Card(2, 5)],
+                [Card(1, 6), Card(2, 7)],
+                [Card(9, 10)]
+            ]
+        )
+        game.log_manager = make_log_manager("")
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == [ArashiStep]
+        assert is_equal_candidates(
+            step.get_candidates(game),
+            [
+                "0:arashiindex:-1#0",
+                "0:arashiindex:1#0",
+                "0:arashiindex:2#0"
+            ]
+        )
+
+    def test_process_log_3(self, get_step_classes, make_log_manager):
+        step = ArashiStep(0, 0, 0)
+        game = self.get_game(
+            [
+                [Card(1, 1), Card(2, 2), Card(3, 3)],
+                [Card(1, 4)],
+                [Card(2, 5)],
+                [Card(1, 6), Card(2, 7)],
+                [Card(9, 10)]
+            ]
+        )
+        game.log_manager = make_log_manager(
+            "A draws 星屑."
+        )
+        next_steps = step.process(game)
+        assert get_step_classes(next_steps) == []
